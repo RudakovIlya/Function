26b32334286b93c4b1ec7010f29e3b45b517bc18
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-20 22:48:35 +0300

    feat: add solution task 28

--- unit_03/unit_03.js
+++ unit_03/unit_03.js
@@ -448,6 +448,9 @@ document.querySelector('.b-27').addEventListener('click', function () {
 // Напишите функцию которая принимает два аргумента - строку и подстроку и если подстрока есть в строке - возвращает true, если нет false.
 
 function t28(str, substr) {
+
+    return str.includes(substr);
+
 }
 
 document.querySelector('.b-28').addEventListener('click', function () {
