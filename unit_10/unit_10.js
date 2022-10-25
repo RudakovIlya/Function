6b28c7e520575c0bd36b488bbf9eee47e97ba78e
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-25 14:54:15 +0300

    feat: add solution task 8

--- unit_10/unit_10.js
+++ unit_10/unit_10.js
@@ -203,7 +203,23 @@ document.querySelector('.b-7').addEventListener('click', () => {
 // Task 8.
 // Напишите рекурсивную функцию t8, которая получает с помощью randomInt целое число от 1000 до 9000 и проверяет если сумма первых двух цифр числа равна сумме 3 и 4 числа то возвращает это число. Если нет - повторяет операцию. Например число 1235 не удовлетворяет этому условию, потому что 1+2 не равно 3+5. А вот число 7180  - удовлетворяет.
 
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
 function t8() {
+
+    const rand = randomInteger(1000, 9000);
+
+    const str = rand.toString().slice(0, 2);
+
+    if (+str[0] + +str[1] === 3 || +str[0] + +str[1] === 4) {
+        return rand;
+    } else {
+        console.log(rand);
+        return t8();
+    }
 
 }
 
