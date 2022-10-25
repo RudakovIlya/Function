6ac57cddf70c516bc6eb19fe7e8fdcaf44ecff87
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-25 13:48:32 +0300

    feat: add soluiton task 5

--- unit_10/unit_10.js
+++ unit_10/unit_10.js
@@ -113,6 +113,19 @@ let ar5 = [];
 
 function t5() {
 
+    const rand = randomInteger(0, 10);
+
+    ar5.push(rand);
+
+    const arnew = ar5.reduce((accum, item) => accum += item);
+
+    if (arnew >= 30) {
+        return ar5;
+    } else {
+        console.log(ar5);
+        return t5()
+    }
+
 }
 
 document.querySelector('.b-5').addEventListener('click', () => {
